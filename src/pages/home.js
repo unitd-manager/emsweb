98517fa57a14687c99c51863cc3774cdff397644
Sulfiya786 <sulfiya@unitdtechnologies.com>
2024-04-17 19:33:30 +0530

    deploy

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,15 @@
 import React, { useEffect,useState } from 'react';
-
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Link} from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import api from '../constants/api';
-import HeroSliderTwo from '../components/HeroSliderTwo';
 import EventSlider from '../components/EventSlider';
 
 const Home = () => {
     // const [banners, setBanners] = useState([]);
       const [videoUrls, setVideoUrls] = useState([]);
-
+      const [banners, setBanner] = useState([]);
     // Function to fetch video URLs from the API
     const getVideoUrls = () => {
       api
@@ -25,8 +22,54 @@ const Home = () => {
           // Handle error
         });
     };
-
-
+    const getBanners = () => {
+        // First API call to get banners
+        api.get("/content/getBanners")
+          .then((res) => {
+            setBanner(res.data.data);
+      
+            // Extract content_id from the response
+            const contentIds = res.data.data.map(item => item.content_id);
+      
+            // Second API call to get images based on content_id
+            contentIds.forEach(contentId => {
+              // Making a POST request with content_id in the request body
+              api.post("/file/getListOfFiles", { record_id: contentId })
+                .then((imageRes) => {
+                  // Handle image response
+                  console.log(`Images for content_id ${contentId}:`, imageRes.data);
+                })
+                .catch((imageError) => {
+                  console.error(`Error fetching images for content_id ${contentId}:`, imageError);
+                });
+            });
+          })
+          .catch((error) => {
+            console.error("Error fetching banners:", error);
+          });
+      };
+      const bannersettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1, // Display three slides at a time
+        slidesToScroll: 1, // Scroll one slide at a time
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 1140,
+            settings: {
+              slidesToShow: 2,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+            },
+          },
+        ],
+      };
     
 // const sliderSettings = {
 //     dots: true,
@@ -63,6 +106,7 @@ const Home = () => {
 useEffect(() => {
 // getBannerImages();
 getVideoUrls(); 
+getBanners();
 }, []);
   return (
     
@@ -198,11 +242,26 @@ getVideoUrls();
             </div>
         </div>
     </div>
-   
-  
+   <div>
+   <Slider {...bannersettings}>
+        {Array.isArray(banners) &&
+          banners.map((item, index) => (
+            <div key={item.content_id} className="single-blog">
+              <div className="part-img">
+                <img
+                  src={`https://emsweb.unitdtechnologies.com/storage/uploads/${item.file_name}`}
+                  alt={`News ${item.content_id}`}
+                  style={{ width: "100%", height: "400px", objectFit: "cover" }}
+                />
+              </div>
+              <div className="part-txt">
+                {/* Check if item.description is not null before accessing its properties */}
+              </div>
+            </div>
+          ))}
+      </Slider>
 
-<HeroSliderTwo />
-<EventSlider></EventSlider>
+    <EventSlider></EventSlider>
        
 
  {/* Video Gallery Panel */}
@@ -226,7 +285,65 @@ getVideoUrls();
         </div>
       </div>
 
-   
+      <div class="col-xl-12 col-lg-12 col-md-9">
+                    <div class="tab-content" id="nav-tabContent">
+                        <div class="tab-pane fade show active" id="nav-month" role="tabpanel" aria-labelledby="nav-month-tab">
+                            <div class="row">
+                                <div class="col-xl-6 col-lg-6">
+                                    <div class="single-box">
+                                        <div class="part-img">
+                                            <img src="assets/images/pricing-img-1.jpg" alt="image"/>
+                                           
+                                        </div>
+                                        
+                                        <div class="part-btn" style={{marginTop:"25px"}}>
+                                            <a href="#" class="def-btn">Buy Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6">
+                                    <div class="single-box">
+                                        <div class="part-img">
+                                            <img src="assets/images/pricing-img-2.jpg" alt="image"/>
+                                           
+                                        </div>
+                                      
+                                        <div class="part-btn"style={{marginTop:"25px"}}>
+                                            <a href="#" class="def-btn">Buy Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="nav-year" role="tabpanel" aria-labelledby="nav-year-tab">
+                            <div class="row">
+                                <div class="col-xl-6 col-lg-6">
+                                    <div class="single-box">
+                                        <div class="part-img">
+                                            <img src="assets/images/pricing-img-1.jpg" alt="image"/>
+                                        </div>
+                                      
+                                        <div class="part-btn">
+                                            <a href="#" class="def-btn">Buy Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6 col-lg-6">
+                                    <div class="single-box">
+                                        <div class="part-img">
+                                            <img src="assets/images/pricing-img-2.jpg" alt="image"/>
+                                           
+                                        </div>
+                                      
+                                        <div class="part-btn">
+                                            <a href="#" class="def-btn">Buy Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
     <div class="feature">
         <div class="container">
             <div class="row justify-content-center">
@@ -814,7 +931,7 @@ getVideoUrls();
             </div>
         </div>
     </div>
-  
+  </div>
     <div class="footer">
         <div class="container">
             <div class="main-footer">
