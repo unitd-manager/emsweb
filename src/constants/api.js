a5cc5c1f2ca446cf5ce3528078dbb5e125e44db0
Sabina <sabina@unitdtechnologie.com>
2024-04-26 11:24:23 +0530

    api

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,13 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-<<<<<<< HEAD
 //baseURL: 'https://emsweb.unitdtechnologies.com:4014',
 baseURL: 'http://localhost:5009'
-=======
-// baseURL: 'https://emsweb.unitdtechnologies.com:4014',
-baseURL: 'http://localhost:5009',
->>>>>>> 9819bd79cc24d065385e4def6a9c0531ac8cfef9
 });
 
 
